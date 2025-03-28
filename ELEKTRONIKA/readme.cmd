7f1e5b8ea7d015b0a8ccd41271f2f2fac2eb0af6
Karel <145646267+Karel122@users.noreply.github.com>
2025-03-28 18:41:25 +0100

    Update readme.cmd

--- ELEKTRONIKA/readme.cmd
+++ ELEKTRONIKA/readme.cmd
@@ -1,1 +1,1 @@
-Knjižnjice za esp32 (wifi) lahko najdeš na tej povezavi: https://www.dropbox.com/scl/fo/ys76epnqdnaju8tonx55v/ACTJryge6S2RD3JwYpuyCzE?rlkey=yrljitgfxrg8z1tzfxtl0xqzx&e=1&dl=0
+Knjižnice za esp32 (wifi) lahko najdeš na tej povezavi: https://www.dropbox.com/scl/fo/ys76epnqdnaju8tonx55v/ACTJryge6S2RD3JwYpuyCzE?rlkey=yrljitgfxrg8z1tzfxtl0xqzx&e=1&dl=0
